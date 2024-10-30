79373f253d1a44e0cfd249228eb41fe318ed32be
Arhab Saufi <arhabsaufi@gmail.com>
2024-10-30 13:19:08 +0700

    Add files via upload

---
+++ p_post.php
@@ -0,0 +1,21 @@
+<?php
+$_name = $_POST['name'];
+$_email = $_POST['email'];
+$_website = $_POST['website'];
+$_comment = $_POST['comment'];
+$_gender = $_POST['gender'];
+
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+
+?>
+<br><br>
+<a href="post.php">Back</a>
